4621ebc0b708cd84db0883c184abaf544a2166a2
Marc <marcortizgonzalez2002@gmail.com>
2021-12-03 18:54:44 +0100

    Errores resueltos

--- BBDD/bd_inscripciones.sql
+++ BBDD/bd_inscripciones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2021 a las 15:30:36
+-- Tiempo de generación: 03-12-2021 a las 18:54:05
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.4.24
 
@@ -20,14 +20,12 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_inscripciones`
 --
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_admin`
 --
-
-CREATE DATABASE bd_inscripciones;
-USE bd_inscripciones;
 
 CREATE TABLE `tbl_admin` (
   `id_admin` int(11) NOT NULL,
@@ -68,10 +66,10 @@ CREATE TABLE `tbl_eventos` (
 --
 
 INSERT INTO `tbl_eventos` (`id_ev`, `nombre_ev`, `desc_ev`, `img_ev`, `edad_ev`, `capacidad_ev`, `fecha_ev`, `direccion_ev`, `telf_contacto_ev`) VALUES
-(1, 'Cursa dels Nassos', 'La Cursa dels Nassos es una carrera atlética de carácter popular, de 10 kilómetros de recorrido, que tiene lugar el 31 de diciembre de cada año (día de San Silvestre) en Barcelona, España. Empezó a disputarse en 1999 como San Silvestre Barcelonesa, hasta que en 2004 tomó la denominación actual.\\r\\nLa carrera está organizada por el Ayuntamiento de Barcelona, a través del Institut Barcelona Esports, y cuenta con la dirección técnica de la Agrupació Atlètica Catalunya.', '../img/25-11-21-cursa_nassos.png', 18, 10000, '2021-12-31', 'Calle de Selva de Mar', 932812841),
+(1, 'Cursa dels Nassos', 'La Cursa dels Nassos es una carrera atlética de carácter popular, de 10 kilómetros de recorrido, que tiene lugar el 31 de diciembre de cada año (día de San Silvestre) en Barcelona, España. Empezó a disputarse en 1999 como San Silvestre Barcelonesa, hasta que en 2004 tomó la denominación actual.La carrera está organizada por el Ayuntamiento de Barcelona, a través del Institut Barcelona Esports, y cuenta con la dirección técnica de la Agrupació Atlètica Catalunya.', '../img/25-11-21-cursa_nassos.png', 18, 10000, '2021-12-31', 'Calle de Selva de Mar', 932812841),
 (2, 'La Cursa El Corte Inglés', 'La Cursa El Corte Inglés, popularmente conocida como \"La Cursa\", es una carrera atlética de carácter popular que tiene lugar cada año, desde 1979, en un circuito urbano en las calles de Barcelona. Está organizada por la empresa El Corte Inglés, con el apoyo del Ayuntamiento de Barcelona.', '../img/25-11-21-cursa_corteingles.jpg', 6, 100000, '2022-04-03', 'Parque de la Ciudadela', 932701730),
-(3, 'Cursa de les Empreses', '!Ya tenemos fecha para la Cursa de les Empreses!\r\n\r\nEl 17 de diciembre de 2021 no dudes en participar con tus compañeros de trabajo en una prueba que te hará disfrutar de un recorrido llano y sin dificultad técnica.\r\n\r\nCompite por equipos de 2, 3 o 4 participantes de la misma empresa, o si eres más atrevido corre con traje de manera individual en la categoría executive!\r\n\r\n¡Una experiencia difícil de repetir y que hacen que la prueba se convierta en única!', '../img/25-11-21-cursa_empreses.jpg', 18, 35000, '2021-12-17', 'Plaza Europa', 931594040),
-(4, 'Maratón de Barcelona', 'Corre el maratón más mágico y completo del mundo: urbano, animado, mediterráneo, monumental, escénico, rápido y accesible para todos/as.', '../img/26-11-21-marato_bcn_cursa.png', 18, 25000, '2022-04-03', 'Plaça Espanya', 934315533);
+(3, 'Cursa de les Empreses', '!Ya tenemos fecha para la Cursa de les Empreses!El 17 de diciembre de 2021 no dudes en participar con tus compañeros de trabajo en una prueba que te hará disfrutar de un recorrido llano y sin dificultad técnica.Compite por equipos de 2, 3 o 4 participantes de la misma empresa, o si eres más atrevido corre con traje de manera individual en la categoría executive!¡Una experiencia difícil de repetir y que hacen que la prueba se convierta en única!', '../img/25-11-21-cursa_empreses.jpg', 18, 35000, '2021-12-17', 'Plaza Europa', 931594040),
+(4, 'Maratón de Barcelona', 'Corre el maratón más mágico y completo del mundo: urbano, animado, mediterráneo, monumental, escénico, rápido y accesible para todos/as.', '../img/26-11-21-marato_bcn_cursa.png', 18, 4, '2022-04-03', 'Plaça Espanya', 934315533);
 
 -- --------------------------------------------------------
 
@@ -88,6 +86,14 @@ CREATE TABLE `tbl_usuarios` (
   `img_dni_usu` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `tbl_usuarios`
+--
+
+INSERT INTO `tbl_usuarios` (`dni_usu`, `nombre_usu`, `apellido_usu`, `edad_usu`, `email_usu`, `img_dni_usu`) VALUES
+('46480113B', 'Marc', 'Ortiz', 18, '7100.joan23@fje.edu', '../img/dni/3-12-21-horariodaw2.jpg'),
+('46491116C', 'Miguel', 'Gras', 20, 'm@gmail.com', '../img/dni/3-12-21-1556725188.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -97,8 +103,19 @@ CREATE TABLE `tbl_usuarios` (
 CREATE TABLE `tbl_voluntarios` (
   `id_par` int(11) NOT NULL,
   `dni_par` char(9) COLLATE utf8mb4_spanish_ci NOT NULL,
-  `evento_par` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL
+  `evento_par` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `id_ev` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_voluntarios`
+--
+
+INSERT INTO `tbl_voluntarios` (`id_par`, `dni_par`, `evento_par`, `id_ev`) VALUES
+(1, '46491116C', 'Cursa dels Nassos', 1),
+(2, '46491116C', 'La Cursa El Corte Inglés', 2),
+(3, '46480113B', 'La Cursa El Corte Inglés', 2),
+(4, '46480113B', 'Maratón de Barcelona', 4);
 
 --
 -- Índices para tablas volcadas
@@ -142,13 +159,13 @@ ALTER TABLE `tbl_admin`
 -- AUTO_INCREMENT de la tabla `tbl_eventos`
 --
 ALTER TABLE `tbl_eventos`
-  MODIFY `id_ev` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_ev` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `tbl_voluntarios`
 --
 ALTER TABLE `tbl_voluntarios`
-  MODIFY `id_par` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_par` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
